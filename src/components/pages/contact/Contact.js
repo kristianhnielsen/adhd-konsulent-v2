47fe0ab4734926a613cf112136c3b9d32244719a
kristianhnielsen <kristianhnielsen@protonmail.com>
2022-08-26 23:08:33 +0800

    added location under contact + adjusted Contact style

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,30 +1,32 @@
 import Headline from "../../shared/headline/Headline";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import NameCard from "./NameCard";
 import PhoneCard from "./PhoneCard";
 import EmailCard from "./EmailCard";
-import {Container} from "../../styles/Contact.styled";
+import { Container } from "../../styles/Contact.styled";
 import ProfilePicture from "./ProfilePicture";
+import LocationCard from "./LocationCard";
 
 export default function Contact() {
-	return (
-		<>
-			<Helmet>
-				<title>Kontakt | ADHD og Autisme Konsulent</title>
-				<meta
-					name="description"
-					content="Kontakt og bestil tid ved ADHD og autisme specialist Lasse Nielsen"
-				/>
-			</Helmet>
-			<Headline text="Kontakt" />
-			<Container>
-				<section>
-					<NameCard />
-					<PhoneCard />
-					<EmailCard />
-				</section>
-				<ProfilePicture />
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Helmet>
+        <title>Kontakt | ADHD og Autisme Konsulent</title>
+        <meta
+          name="description"
+          content="Kontakt og bestil tid ved ADHD og autisme specialist Lasse Nielsen"
+        />
+      </Helmet>
+      <Headline text="Kontakt" />
+      <Container>
+        <section>
+          <NameCard />
+          <PhoneCard />
+          <EmailCard />
+          <LocationCard />
+        </section>
+        <ProfilePicture />
+      </Container>
+    </>
+  );
 }
